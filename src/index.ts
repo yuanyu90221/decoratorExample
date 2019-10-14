11bfe05455aeffaa381373e75cd4f4c67cef7366
JsonLiang <json@cbx.io>
2019-10-14 14:23:33 +0800

    update server

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,12 @@ let testMul = new DecoratorTest().multply(1,2);
 
 let test = new Greeter("world");
 console.log(test);
+try {
+
+  console.log(test.hello);
+  delete test.hello;
+  // console.log(test.hello);
+  // test.test = 'test';
+} catch(e) {
+  console.error(e);
+}
